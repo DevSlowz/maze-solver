219da2e8ac81302852dfee2efa737eaaad152b28
DevSlowz <msvieira021@gmail.com>
2024-04-28 10:42:11 -0400

    Adjust Line constructor to take complete points

--- graphics.py
+++ graphics.py
@@ -55,14 +55,11 @@ class Point :
         self.y = y 
 
 class Line :
-    def __init__(self, x1, y1, x2, y2):
-        self.x1 = x1
-        self.y1 = y1
-        
-        self.x2 = x2
-        self.y2 = y2
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
 
     def draw(self, canvas, fill_color):
         canvas.create_line(
-            self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
         )
