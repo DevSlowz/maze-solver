8f9d3984cccd9a61ce3164a197a02fe27505550a
DevSlowz <msvieira021@gmail.com>
2024-04-27 22:02:22 -0400

    Adjust line constructor and draw function

--- graphics.py
+++ graphics.py
@@ -52,6 +52,14 @@ class Point :
         self.y = y 
 
 class Line :
-    def __init__(self, pointA, pointB):
-        self.pointA = pointA
-        self.pointB = pointB
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        
+        self.x2 = x2
+        self.y2 = y2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2
+        )
