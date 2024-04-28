dacba52c7fa18a14efc45750d6c175bfb1e8c71b
DevSlowz <msvieira021@gmail.com>
2024-04-27 22:09:33 -0400

    Add point and line instances to test draw_line function

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
-    win.wait_for_close()
+    
 
+    point1 = Point(5,10)
+    point2 = Point(20,60)
+
+    line = Line(point1.x, point1.y, point2.x, point2.y)
+
+    win.draw_line(line, "red")
+
+    win.wait_for_close()
 main()
