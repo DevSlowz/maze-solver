2002ee82d398de9deb1cd8f46e58fb5284f1f32f
DevSlowz <msvieira021@gmail.com>
2024-05-08 16:35:48 -0400

    Develop maze class to take a window and window size and draw cell to populate the windoe to the specified amount

--- maze.py
+++ maze.py
@@ -1,6 +1,8 @@
-from asyncio import sleep
 from cell import Cell
-from graphics import Window
+import random
+import time
+
+
 class Maze:
     def __init__(
         self,
@@ -12,6 +14,7 @@ class Maze:
         cell_size_y,
         win,
     ):
+        self._cells = []
         self._x1 = x1
         self._y1 = y1
         self._num_rows = num_rows
@@ -19,27 +22,50 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
-        self._cells = []
 
+        self._create_cells()
 
     def _create_cells(self):
-        # For every row generate a column populated with cells
-        for row in range(self._num_rows):
-            col = []
-            for column in range(self._num_cols):
-                col.append(Cell(self._win))
+        # Populate every row with a column full of cells
+        for i in range(self._num_cols):
+            col_cells = []
+            for j in range(self._num_rows):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
 
-            self._cells.append(col)
-        # For Every cell draw the cell
+        # For every cell draw the cell 
         for i in range(self._num_cols):
             for j in range(self._num_rows):
                 self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
-        pass
-
+        # Check if the window object is None. If so, stop the function early.
+        if self._win is None:
+            return
+        
+        # Calculate the x-coordinate (x1) of the cell's top-left corner.
+        # It combines the maze's left edge (self._x1) with the horizontal distance based on cell's column.
+        x1 = self._x1 + i * self._cell_size_x
+        
+        # Calculate the y-coordinate (y1) of the cell's top-left corner.
+        # It uses the maze's top edge (self._y1) and the vertical distance based on cell's row.
+        y1 = self._y1 + j * self._cell_size_y
+        
+        # Find the x-coordinate (x2) of the cell's bottom-right corner by adding the cell's width to x1.
+        x2 = x1 + self._cell_size_x
+        
+        # Determine the y-coordinate (y2) of the cell's bottom-right corner by adding the cell's height to y1.
+        y2 = y1 + self._cell_size_y
+        
+        # Use the calculated coordinates to draw the cell at its designated position.
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        
+        # Update the window to reflect the newly drawn cell and potentially pause briefly to allow for animation.
+        self._animate()
 
 
     def _animate(self):
-        self.win.redraw()
-        sleep(0.05)
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.05)
