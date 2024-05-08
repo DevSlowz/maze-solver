9421cf486fbb40003535e603745f93cbb17601f7
DevSlowz <msvieira021@gmail.com>
2024-05-08 17:02:25 -0400

    Adjust draw to draw white lines to indicate an entrace and exit

--- cell.py
+++ cell.py
@@ -15,40 +15,38 @@ class Cell :
         self._win = win
 
         
+
     def draw(self, x1, y1, x2, y2):
-        # Set the coordinates for the rectangle
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
-
-        # Draw the left wall if it exists
         if self.has_left_wall:
-            # Create a line from the top left to the bottom left of the rectangle
             line = Line(Point(x1, y1), Point(x1, y2))
-            # Draw the line on the window
             self._win.draw_line(line)
-
-        # Draw the top wall if it exists
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "white")
         if self.has_top_wall:
-            # Create a line from the top left to the top right of the rectangle
             line = Line(Point(x1, y1), Point(x2, y1))
-            # Draw the line on the window
             self._win.draw_line(line)
-
-        # Draw the right wall if it exists
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
         if self.has_right_wall:
-            # Create a line from the top right to the bottom right of the rectangle
             line = Line(Point(x2, y1), Point(x2, y2))
-            # Draw the line on the window
             self._win.draw_line(line)
-
-        # Draw the bottom wall if it exists
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
         if self.has_bottom_wall:
-            # Create a line from the bottom left to the bottom right of the rectangle
             line = Line(Point(x1, y2), Point(x2, y2))
-            # Draw the line on the window
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
 
     # Draw a path between 2 cells
     def draw_move(self, to_cell, undo=False):
@@ -66,8 +64,7 @@ class Cell :
         fill_color = "red"
         # If the undo flag is set, change the fill color to gray
         if undo:
-            fill_color = "gray"
-
+            fill_color = "white"
         # Create a line from the center of the current cell to the center of the target cell
         line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
         # Draw the line with the specified fill color
